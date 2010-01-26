2972d655fd526ef8f476125db494fdc8d9a62444
Thomas Rueckstiess <ruecksti@in.tum.de>
2010-01-26 15:14:43 +0100

    this tool plots the results of all experiments simultaneously, updating the plot whenever a new value has been calculated. It indicates new values with a red plot curve, that turns gray again after a few seconds.

--- expplot.py
+++ expplot.py
@@ -2,17 +2,7 @@ from expsuite import ExperimentSuite
 from matplotlib import pyplot as plt
 from numpy import *
 import time
-
-suite = ExperimentSuite()
-
-
-# get all experiments
-experiments = suite.get_exps()
-shape = ceil(sqrt(len(experiments)))
-hgraphs = {}
-
-plt.ion()
-plt.figure(figsize=(16,10))
+import optparse
 
 
 def creategraphs():
@@ -31,7 +21,52 @@ def creategraphs():
         plt.gcf().canvas.draw()
 
 
+
+def parse_opt():
+    """ parses the command line options for different settings """
+    optparser = optparse.OptionParser()
+    # optparser.add_option('-c', '--config',
+    #     action='store', dest='config', type='string', default='experiments.cfg', 
+    #     help="your experiments config file")
+    # optparser.add_option('-n', '--numcores',
+    #     action='store', dest='ncores', type='int', default=cpu_count(), 
+    #     help="number of processes you want to use, default is %i"%cpu_count())  
+    # optparser.add_option('-d', '--del',
+    #     action='store_true', dest='delete', default=False, 
+    #     help="delete experiment folder if it exists")
+    # optparser.add_option('-p', '--progress',
+    #     action='store_true', dest='progress', default=False, 
+    #     help="observe progress of the experiments interactively")
+    # optparser.add_option('-b', '--browse',
+    #     action='store_true', dest='browse', default=False, 
+    #     help="browse experiments in config file.")      
+    # optparser.add_option('-B', '--Browse',
+    #     action='store_true', dest='browse_big', default=False, 
+    #     help="browse experiments in config file, more verbose than -b")      
+
+    options, args = optparser.parse_args()
+    return options, args
+
 if __name__ == '__main__':
+    
+    options, args = parse_opt()
+    
+    suite = ExperimentSuite()
+    
+    # get all experiments
+    path = '.'
+    if len(args) > 0:
+        path = args[0]
+
+    experiments = suite.get_exps(path)
+    
+    shape = ceil(sqrt(len(experiments)))
+    hgraphs = {}
+
+    plt.ion()
+    plt.figure(figsize=(16,10))
+
+    
     creategraphs()
     while True:
         for i, exp in enumerate(experiments):
