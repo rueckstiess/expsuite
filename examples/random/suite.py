d1024d647f60d64ab5da93f25b3e2d54d633ccb2
Thomas Rückstieß <thomas@rueckstiess.net>
2010-10-05 16:21:57 +0200

    added explanation and comments to this example.

--- examples/random/suite.py
+++ examples/random/suite.py
@@ -1,3 +1,34 @@
+#############################################################################
+#
+# Example: Random Numbers (random) 
+# 
+# This example demonstrates how to verify a a simple stochastic law,
+# namely that the sample mean of random numbers drawn from a normal dis-
+# tribution does converge to the actual mean of the distribution. We use
+# the random module of the numpy package and store the drawn numbers in
+# a numpy array.
+# 
+# Furthermore, this example makes use of the optional restore support.
+# The restore_supported flag is set to True and the two functions
+# save_state() and restore_state() are implemented. If the experiment
+# is interrupted during a repetition, it will continue exactly from
+# where it left off. (The default behavior was to restart the whole
+# repetition and delete all iterations that were already executed).
+#
+# Run this script from the command line: python suite.py
+#
+# The results can be plotted with the matplotlib module. If it is
+# available on your machine, run the plotresults.py script from the
+# command line and look at the plots.
+#
+# For more information on the restore support functionality, refer to
+# the documentation.pdf file, Section 3.4. The API interface is explained
+# in more detail in Section 6.
+#
+# Copyright (c) 2010 - Thomas Rueckstiess
+#
+#############################################################################
+
 from expsuite import PyExperimentSuite
 from numpy import *
 import os
